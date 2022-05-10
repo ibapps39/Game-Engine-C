a19472ed6331b88cddad135b353dfb22d92dd144
Ian Brown <ibapps39@gmail.com>
2022-05-09 19:49:43 -0600

    update

---
+++ State Manager/README.md
@@ -0,0 +1,11 @@
+# State Manager
+## A Stack Based Architecture
+- ### What is a state manager? 
+    - A state manager is a stack based architecture that allows for multiple states to be loaded and unloaded at the same time.
+- ### What is a state?
+    - A state is a collection of data that is loaded and unloaded at the same time.
+- ### Needed Components - Functions, Pointers, and more
+    - [ ] Initialize game state
+    - [ ] Delete state 
+    - [ ] Update state
+    - [ ] Draw state
---
+++ StateManager.h
@@ -0,0 +1,29 @@
+#ifndef STATE_MANAGER_H
+#define STATE_MANAGER_H
+/* State Strucutre defined as Init, Update, Draw, Destroy */
+//Function pointer to the state's functions
+typedef unsigned int (*fnPtr)();
+typedef unsigned int (*fnPtrFl)(float);
+
+/* Define State: Init, Update, Draw, Destroy */
+typedef struct {
+    fnPtr init;
+    fnPtr update;
+    fnPtr draw;
+    fnPtr destroy;
+} State;
+
+/*Define State Manager: 
+a stack,
+ a capacity of states, 
+ and a current state as the index of the top of the stack*/
+
+ typedef struct {
+     State **stack;
+     int capacity;
+     int top;
+ } StateManager;
+int STATEMANAGER_init(StateManager *statemanager);
+
+
+#endif // STATE_MANAGER_H
