5cc3baeb5eb97bdd473547ce6dbb811ace94c1f8
Ian Brown <ibapps39@gmail.com>
2022-05-17 22:44:00 -0600

    graphics

--- Graphic_Init/include/Graphics.h
+++ Graphic_Init/include/Graphics.h
@@ -7,12 +7,21 @@
 /*
 Defining a SDL Window, SDL init function, and SDL_free function
 */
+
+/*
+Dimensional struct for a SDL Window
+*/
 typedef struct {
   int width;
   int height;
   SDL_Window *window;
 } Graphics;
-
+/*
+Initializes a SDL Window and returns 0 if successful
+*/
 int GRAPHIC_init(Graphics *graphics);
+/*
+Frees allocated memory for a SDL Window
+*/
 void GRAPHICS_free(Graphics *graphics);
 #endif //ENGINE_GRAPHICS_H
---
+++ Graphic_Init/src/Graphics.c
@@ -0,0 +1,23 @@
+#include "../include/Graphics.h"
+
+int GRAPHICS_init(Graphics *graphics){
+    graphics->window = SDL_CreateWindow(
+        "C Engine With SDL2",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_SHOWN
+    );
+
+    if(graphics->window == NULL){
+        printf("Error: %s\n", SDL_GetError());
+        SDL_Log("Unable to create window: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    return 0;
+}
+void GRAPHICS_free(Graphics *graphics){
+    SDL_DestroyWindow(graphics->window);
+}
